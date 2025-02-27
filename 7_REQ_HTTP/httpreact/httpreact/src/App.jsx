bd27727888d48207dfa6554a09eeecf8fc4ddfcd
tozzicc <tozzicc@gmail.com>
2025-02-27 17:38:07 -0300

    O

--- 7_REQ_HTTP/httpreact/httpreact/src/App.jsx
+++ 7_REQ_HTTP/httpreact/httpreact/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+
+import { useState, useEffect } from "react";
+
+const url = "http://localhost:3000/products";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [products, setProducts] = useState([]); // Salvando os produtos
+
+  // 1 - Resgatando dados
+  useEffect(async () => {
+    const res = await fetch(url); // Recebendo a requisição
+
+    const data = await res.json(); // Transformando o json
+
+    setProducts(data);
+  });
+
+  console.log(products);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <h1>Lista de Produtos</h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
